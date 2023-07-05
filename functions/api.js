75a6102c172c081a7f14831af090c1732ec0edd7
51702193 <51702193@student.tdtu.edu.vn>
2023-07-05 14:25:34 +0700

    asdasd

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
 const serverless = require("serverless-http");
 const cors = require("cors");
+
+const { initDb: mongoRun } = require("../mongodb");
 
 require("dotenv/config");
 
@@ -11,32 +14,31 @@ const wardList = require("../seed-data/wardList.json");
 const app = express();
 const router = express.Router();
 
-console.log(process.env.MODE);
+// console.log(process.env.MODE);
 
-var whitelist = ["https://dacntt2-n092-fe.netlify.app"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    console.log("run2");
-    callback(null, true);
+// var whitelist = ["https://dacntt2-n092-fe.netlify.app"];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     callback(null, true);
 
-    if (process.env.MODE === "LOCAL" || process.env.MODE === "DEV") {
-      console.log("run");
-      callback(null, true);
-      return;
-    }
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+//     if (process.env.MODE === "LOCAL" || process.env.MODE === "DEV") {
+//       callback(null, true);
+//       return;
+//     }
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
-//Get all students
-router.get("/", (req, res) => {
-  res.send("App is running..");
+router.get("/init", async (req, res, next) => {
+  const result = await mongoRun()
+    .then((e) => res.json({ message: e }))
+    .catch((e) => res.status(400).json({ message: e }));
 });
 
 router.get("/districts", (req, res) => {
@@ -49,17 +51,17 @@ router.get("/wards", (req, res) => {
   res.send(wardList);
 });
 
-//delete existing record
-router.delete("/", (req, res) => {
-  res.send("Deleted existing record");
-});
+// //delete existing record
+// router.delete("/", (req, res) => {
+//   res.send("Deleted existing record");
+// });
 
-//updating existing record
-router.put("/", (req, res) => {
-  res.send("Updating existing record");
-});
+// //updating existing record
+// router.put("/", (req, res) => {
+//   res.send("Updating existing record");
+// });
 
-// app.use("/api", router);
+// // app.use("/api", router);
 app.use("/.netlify/functions/api", router);
 
 if (process.env.MODE === "LOCAL") {
---
+++ mongodb/index.js
@@ -0,0 +1,98 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const districtList = require("../seed-data/districtList.json");
+const provinceList = require("../seed-data/provinceList.json");
+const wardList = require("../seed-data/wardList.json");
+
+const uri =
+  "mongodb+srv://51702193:vista1406@dacntt2cluster.ep5jzno.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    // return "Pinged your deployment. You successfully connected to MongoDB!";
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+// run().catch(console.dir);
+
+////////////////
+
+const dbConfig = {
+  database: "DACNTT2",
+  collections: [
+    { name: "provinces", options: {}, seed: provinceList },
+    { name: "districts", options: {}, seed: districtList },
+    { name: "wards", options: {}, seed: wardList },
+  ],
+};
+
+async function initDb() {
+  // const db = await (await client.connect()).db(dbConfig.database);
+  try {
+    await client.connect();
+    const db = client.db(dbConfig.database);
+
+    const collectionsExists = await db.listCollections().toArray();
+    await Promise.all(
+      collectionsExists?.map(async (collection) => {
+        await db.dropCollection(collection.name);
+      })
+    );
+
+    await Promise.all(
+      dbConfig.collections?.map(async (collection) => {
+        await db.createCollection(collection.name, collection.options);
+        await db.collection(collection.name).insertMany(collection.seed || []);
+      })
+    );
+
+    return "successfully";
+  } finally {
+    await client.close();
+  }
+}
+
+async function initDb() {
+  // const db = await (await client.connect()).db(dbConfig.database);
+  try {
+    await client.connect();
+    const db = client.db(dbConfig.database);
+
+    const collectionsExists = await db.listCollections().toArray();
+    await Promise.all(
+      collectionsExists?.map(async (collection) => {
+        await db.dropCollection(collection.name);
+      })
+    );
+
+    await Promise.all(
+      dbConfig.collections?.map(async (collection) => {
+        await db.createCollection(collection.name, collection.options);
+        await db.collection(collection.name).insertMany(collection.seed || []);
+      })
+    );
+
+    return "successfully";
+  } finally {
+    await client.close();
+  }
+}
+
+module.exports = { initDb };
